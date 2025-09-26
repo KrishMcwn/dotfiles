ba511a956db7a06974b2f984707acffc86764ab0
krishmcwn <krishmcwn@gmail.com>
2025-09-26 23:13:25 +0100

    intial commit

---
+++ nvim-dot/lua/custom/chadrc.lua
@@ -0,0 +1,9 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'penumbra_dark',
+  transparency = true}
+
+M.plugins = "custom.plugins"
+
+return M
---
+++ nvim-dot/lua/custom/plugins.lua
@@ -0,0 +1,36 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "pyright",
+        "mypy",
+        "ruff",
+      },
+    },
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+}
+
+return plugins
